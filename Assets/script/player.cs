0b5531e27d5b31535d803a9540549e3dcef25c77
MSI\kengo <kengo20100302@gmail.com>
2026-06-22 13:02:23 +0900

    2026/6/22 更新

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,6 +18,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private Vector3 loolAngls;
     private float gyroAngls;
 
+    //バリア設定
+    [Header(" * * * バリアの設定")]
+    public GameObject barrire;           //バリアオブジェクトの参照
+    public MeshRenderer barrireRenderer; //バリアのレンダラー参照
+    public bool barrireActivation;　     //バリアのフラグ
 
     void Start()
     {
@@ -32,9 +38,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
         loolAngls.x += inputMoveVelocity.y;
         loolAngls.y += inputMoveVelocity.x;
 
-        loolAngls.x = Mathf.Clamp(loolAngls.x, -15,15);
-        loolAngls.y = Mathf.Clamp(loolAngls.y, -15,15);
-        gyroAngls = Mathf.Clamp(gyroAngls, -15,15);
+        loolAngls.x = Mathf.Clamp(loolAngls.x, -15, 15);
+        loolAngls.y = Mathf.Clamp(loolAngls.y, -15, 15);
+        gyroAngls = Mathf.Clamp(gyroAngls, -15, 15);
 
         //角度の代入
         lookAxis.transform.eulerAngles = loolAngls;
@@ -42,7 +48,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         loolAngls = Vector3.Lerp(loolAngls, Vector3.zero, Time.deltaTime);
         gyroAngls = Mathf.Lerp(gyroAngls, 0, Time.deltaTime);
-        
+
 
 
     }
@@ -56,7 +62,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         //第一引数playerinputから渡された
         if (transform.position.x + value.Get<Vector2>().x < -8 ||
-             transform.position.x + value.Get<Vector2>().x > 8) 
+             transform.position.x + value.Get<Vector2>().x > 8)
             return;
 
         if (transform.position.y + value.Get<Vector2>().y < -4 ||
@@ -102,5 +108,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
         rb.AddForce(Vector3.forward * 50, ForceMode.Impulse);
 
         Destroy(bullet, 3f);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag.Equals("Item/Barrire"))
+        {
+
+            Material m = barrireRenderer.material;
+
+            barrireActivation = true;
+
+            m.SetInt("_IsActive", 1);
+
+        }
+
     }
 }
